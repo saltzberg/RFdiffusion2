8560ab61d2fc928018f63826e6cf7a7cce57e175
saltzberg <dargason@gmail.com>
2025-10-30 02:26:43 +0000

    add multirun to exec command

--- runcommand.sh
+++ runcommand.sh
@@ -4,4 +4,4 @@ apptainer exec --nv rf_diffusion/exec/bakerlab_rf_diffusion_aa.sif \
   --config-name=open_source_demo \
   sweep.benchmarks=$(seq -f tri_sweep_%02g -s, 0 19) \
   outdir=./triad_sweep20
-
+  --multirun
