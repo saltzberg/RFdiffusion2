4d686c3fbc831f7fab7326ce7d764027a4c9f224
saltzberg <dargason@gmail.com>
2025-10-30 02:20:32 +0000

    add test protease input/output

---
+++ runcommand.sh
@@ -0,0 +1,7 @@
+export MKL_THREADING_LAYER=GNU
+apptainer exec --nv rf_diffusion/exec/bakerlab_rf_diffusion_aa.sif \
+  rf_diffusion/benchmark/pipeline.py \
+  --config-name=open_source_demo \
+  sweep.benchmarks=$(seq -f tri_sweep_%02g -s, 0 19) \
+  outdir=./triad_sweep20
+
