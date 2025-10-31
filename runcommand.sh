34d51b51b675ec2fc7a3f61ce5af218cf4acd81e
saltzberg <dargason@gmail.com>
2025-10-31 16:18:17 +0000

    new input and output for 8j4x

--- runcommand.sh
+++ runcommand.sh
@@ -2,6 +2,5 @@ export MKL_THREADING_LAYER=GNU
 apptainer exec --nv rf_diffusion/exec/bakerlab_rf_diffusion_aa.sif \
   rf_diffusion/benchmark/pipeline.py \
   --config-name=open_source_demo \
-  sweep.benchmarks=$(seq -f tri_sweep_%02g -s, 0 19) \
-  outdir=./triad_sweep20
-  --multirun
+  sweep.benchmarks=\'8jx4_native,8jx4_weird\' \
+  outdir=./8jx4_2
